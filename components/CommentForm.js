45434fd56ccb55c285ab2d6d7a1d9f35d2aa507f
eomji <anipap2@naver.com>
2021-03-01 23:27:14 +0900

    nodebird-front Section2-redux5

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -5,15 +5,17 @@ import { useSelector } from "react-redux";
 import useInput from "../hooks/useInput";
 
 const CommentForm = ({ post }) => {
-  const id = useSelector((state) => state.User.me?.id); // 로그인 안해서 me가 없는 상황 대비
+  const id = useSelector((state) => state.user.me?.id); // 로그인 안해서 me가 없는 상황 대비
   const [commentText, onChangeCommentText] = useInput("");
   const onSubmitComment = useCallback(() => {
     console.log(post.id, commentText);
   }, [commentText]);
   return (
     <Form onFinish={onSubmitComment}>
-      <Form.Item>
-        <Input.Textarea
+      <Form.Item style={{ position: "relative", margin: 0 }}>
+        {/* inline 스타일이 좋은건 아니지만, 그렇다고 굳이 모든걸 섣불리 최적화해줄 필요는 x (성능에 문제가 생기기 전까진)
+            왜냐면 얘네를 모두 useMemo로 빼주기엔 너무 많아! */}
+        <Input.TextArea
           value={commentText}
           onChange={onChangeCommentText}
           rows={4}
--- components/PostImages.js
+++ components/PostImages.js
@@ -1,7 +1,26 @@
 import PropTypes from "prop-types";
+import { useCallback, useState } from "react";
 
 const PostImages = ({ images }) => {
-  console.log(images);
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        {/* role="presentation" 이 있으면
+            스크린 리더가 -> '이거 클릭할 수는 있지만 굳이 클릭할 필요는 없어!' 라고 알아들음. */}
+      </>
+    );
+  }
   return <div>구현중..</div>;
 };
 
