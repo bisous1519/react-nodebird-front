df30b8820892e762d04e72b9fa86f27b6926bc62
eomji <anipap2@naver.com>
2021-03-04 23:14:56 +0900

    nodebird-front Section3-redux-saga2

--- sagas/index.js
+++ sagas/index.js
@@ -1,5 +1,6 @@
-export default function* rootSaga() {
-  /*
+import { all, fork, call, put } from "redux-saga/effects";
+
+/*
     제너레이터 : 함수의 일종
     const gen() = function*() {
       console.log(1);
@@ -13,5 +14,7 @@ export default function* rootSaga() {
     generator.next() 해서 실행할 수 있음.
     실행때마다 1, 2, 3이 나옴
     -> 제너레이터는 중단점(yield)이 있는 곳에서 멈추는 함수다!!
-  */
+*/
+export default function* rootSaga() {
+  yield all([fork(watchLogin)]);
 }
